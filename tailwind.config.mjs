54d3597b1ae283d5d5e0fb643d98594b8c89b849
henzyd <henzydee@gmail.com>
2025-04-08 22:31:57 +0100

    clean up

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        jost: ["Jost", "sans-serif"],
       },
       screens: {
         smallMobile: { max: "320px" },
